428397f5053cb825d2977b4493f5487094b6c1aa
ryanyunus <ryandinus@gmail.com>
2024-03-27 03:42:12 +0700

    latihan 1 webpro2

---
+++ latihan1.js
@@ -0,0 +1,12 @@
+function checkNumber(tampung) {
+  if (tampung == 42) {
+    console.log("correct");
+  } else {
+    console.log("incorrect");
+  }
+  // TODO: write your code here
+}
+
+checkNumber(3);
+checkNumber(7);
+checkNumber(42);
